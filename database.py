a3866baffbe1d26f5fec18bfa6910137185ac27e
Veliko Kardziev <velikok@gmail.com>
2024-12-26 20:21:15 +0100

    Refactor database connection to use environment variables; remove obsolete migration script

--- database.py
+++ database.py
@@ -1,9 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 engine = create_engine(
-    'mysql+pymysql://root:supersecret@localhost:3306/onleihe_db',
+    DATABASE_URL,
     pool_recycle=3600,
     pool_size=10,
     max_overflow=20
